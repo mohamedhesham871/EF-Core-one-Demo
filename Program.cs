2ec3aff709a92f91a3454317f3ed60dd5929b3d1
20100 <mmmelkady23@gmail.com>
2025-02-26 01:14:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿namespace EF_Core_one__Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Video one
+            //[EXtra Information]
+            /*Object Relational Mapping (ORM) is a technique used in creating a "bridge" between 
+              object-oriented programs and, in most cases, relational databases.*/
+
+            /* When interacting with a database using OOP languages, you'll have to perform different operations 
+             like creating, reading, updating, and deleting (CRUD) data from a database. 
+              By design, you use SQL for performing these operations in relational databases.*/
+
+            /*
+             An ORM tool is software designed to help OOP developers interact with relational databases.
+             So instead of creating your own ORM software from scratch, you can make use of these tools.
+             */
+            #endregion
+            #region Video Two
+            /*compare Dapper, Entity Framework Core, and ADO.NET*/
+
+            //ADO.NET
+            /*
+             ADO.NET is a database access technology that is part of the.NET Framework
+             It provides a set of classes and interfaces that allow .NET applications to interact with databases.
+
+            ADO.NET is a low-level tool,
+             */
+            //EFcore
+            /*
+            ( EF Core) is a high-level ORM (Object-Relational Mapping) tool that allows 
+            .NET applications to interact with databases
+             */
+            //Dapper
+            /*
+              Dapper is built on top of ADO.NET and provides a simple API for database operations. Dapper is designed to be fast and efficient, 
+              which means that it doesn't have some of the features provided by EF Core.
+             */
+            // Ease of Use
+            /*
+          
+            Regarding ease of use, EF Core is the clear winner. EF Core provides a high-level API that abstracts the database operations,
+            making it easier for developers to work with databases. EF Core also supports LINQ, 
+            which allows developers to write queries in C# instead of SQL.
+            Dapper is also easy to use but requires developers to write SQL queries.
+             */
+            //Dapper more Fast 
+            #endregion
+            #region Video Three
+
+            #endregion
+        }
+    }
+}
